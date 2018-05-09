76070526a2a80829054eb4ff3face52594dda814
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-05-09 11:12:12 +0300

    some ui enhancement (background, header)

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,13 +1,20 @@
 import React from 'react';
 // components
 import UsersListRow from './components/UsersListRow';
+// styles
+import styles from './Users.css';
+import classnames from 'classnames';
 // data
 import users from './users.json';
+
+const cn = classnames.bind(styles);
 
 const Users = () => (
   <div>
     <h2>Users List</h2>
-    {users.map((user, index) => <UsersListRow user={user} key={index} />)}
+    <div className={cn('users-table')}>
+      {users.map((user, index) => <UsersListRow user={user} key={index} />)}
+    </div>
   </div>
 );
 
